6f33effe324f03a5ed3a959b21a5b8eb697496e8
Jayavarshini-Srinivasan <sjayavarshini.aiml2024@citchennai.net>
2026-01-16 21:59:14 +0530

    linked the hackathon website

--- src/components/PrimeDirectivesSection.tsx
+++ src/components/PrimeDirectivesSection.tsx
@@ -25,7 +25,7 @@ export function PrimeDirectivesSection() {
         description: 'Will Be Updated Soon',
         image: 'https://images.unsplash.com/photo-1504384308090-c894fdcc538d?q=80&w=1080',
         icon: Code,
-        route: '/hackathon'
+        route: 'https://athera-hackathon.vercel.app/'
     };
 
     return (
